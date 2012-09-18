519645d901ba92e7e2968c6ef407d697b1027c90
Max <mgreenwald13@kentdenver.org>
2012-09-18 15:11:52 -0600

    made test class

---
+++ src/generalChess/testClass.java
@@ -0,0 +1,12 @@
+package generalChess;
+
+public class testClass {
+	public static void main (String[] args)
+	{
+		Board b= new Board();
+		Bobby BoardLords=new Bobby(true, b);
+	
+		
+		System.out.print(BoardLords.getMoves());
+	}
+}
